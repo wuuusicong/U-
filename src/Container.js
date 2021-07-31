bd557a05ffcba409d3a30cd8a31f6db932b5dc97
DaybydayToAspiration <wuuusicong@gmail.com>
2021-07-31 22:42:31 +0800

    router

---
+++ src/Container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from "./Footer";
+
+const Container = () => {
+    return (
+        <div>
+            <div></div>
+            <div id="footer">
+                <Footer/>
+            </div>
+        </div>
+    )
+}
+export default Container;
